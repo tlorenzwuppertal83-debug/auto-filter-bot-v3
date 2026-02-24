1dcb3635512eb5117cbc5e6585bccf7619eafffd
tlorenzwuppertal83-debug <tlorenzwuppertal83@gmail.com>
2026-02-24 21:24:42 +0100

    config.py aktualisieren

--- config.py
+++ config.py
@@ -1,18 +1,3 @@
-#!/usr/bin/env python3
-# Copyright (C) @ZauteKm
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 import re
 import os
 from os import environ
@@ -20,7 +5,7 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'auto-filter-bot-v3')
+SESSION = environ.get('SESSION', 'telegram-search-bot')
 API_ID = int(environ['API_ID'])
 API_HASH = environ['API_HASH']
 BOT_TOKEN = environ['BOT_TOKEN']
@@ -29,7 +14,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
-BROADCAST_CHANNEL = int(os.environ.get("BROADCAST_CHANNEL", ""))
+BROADCAST_CHANNEL = int(os.environ.get("BROADCAST_CHANNEL", "0"))
 ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", "").split())
 DB_URL = os.environ.get("DATABASE_URI", "")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST", True))
@@ -42,7 +27,7 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('FORCES_SUB')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
-DEV_CHANNEL = "https://t.me/ZauteKm"
+DEV_CHANNEL = ""
 
 # MongoDB information
 DATABASE_URI = environ['DATABASE_URI']
@@ -51,14 +36,13 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 default_start_msg = """
-**Hi, I'm [auto-filter-bot-v3](https://github.com/zautekm/auto-filter-bot-v3)**
-
-Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
+Hi! I am a Telegram Media Search Bot.
+Send me the name of any file and I will try to find it for you.
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
 
 FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
-OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
+OMDB_API_KEY = environ.get("OMDB_API_KEY", "")
 if FILE_CAPTION.strip() == "":
     CUSTOM_FILE_CAPTION=None
 else:
