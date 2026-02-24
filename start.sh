5f9213dd8f3379c6f04fed67d3566517ef31c41f
tlorenzwuppertal83-debug <tlorenzwuppertal83@gmail.com>
2026-02-24 21:30:51 +0100

    start.sh aktualisieren

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
-echo "Cloning Repo...."
-git clone https://github.com/ZauteKm/auto-filter-bot-v3.git /auto-filter-bot-v3
-cd /auto-filter-bot-v3
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+#!/bin/bash
+
+echo "Starting Bot..."
 python3 bot.py
